ee6e31ef349f56a445cc597f92f4a548b32df36f
clement <crousseau@intechinfo.fr>
2015-11-09 17:16:52 +0100

    Création du form , Ajout d'un fichier

---
+++ ITI.Archi-Vite/ITI.Archi-Vite.GUI/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ITI.Archi_Vite.GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ChoseFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ChoseFile
+            // 
+            this.ChoseFile.Location = new System.Drawing.Point(213, 91);
+            this.ChoseFile.Name = "ChoseFile";
+            this.ChoseFile.Size = new System.Drawing.Size(152, 35);
+            this.ChoseFile.TabIndex = 0;
+            this.ChoseFile.Text = "Ajouter un doc";
+            this.ChoseFile.UseVisualStyleBackColor = true;
+            this.ChoseFile.Click += new System.EventHandler(this.ChoseFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 468);
+            this.Controls.Add(this.ChoseFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button ChoseFile;
+    }
+}
+
---
+++ ITI.Archi-Vite/ITI.Archi-Vite.GUI/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace ITI.Archi_Vite.GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChoseFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) 
+            {
+                string path = openFileDialog1.FileName;
+                string path2 = @"C:\ArchiFile\GuillaumeFist3/";
+                string fileName = openFileDialog1.SafeFileName;
+                try
+                {
+                    File.Move(path, path2+fileName);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The process failed: {0}", ex.ToString());
+                }
+            }
+        }
+    }
+}
