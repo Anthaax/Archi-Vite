06504e721d70628b2f662498a14ad91c10dc1fb4
Anthaax <guillaume.fimes@laposte.net>
2015-11-09 16:04:52 +0100

    Creation de patient et de mesages

--- ITI.Archi-Vite/ITI.Archi-Vite.Core/Message.cs
+++ ITI.Archi-Vite/ITI.Archi-Vite.Core/Message.cs
@@ -1,6 +1,50 @@
-﻿namespace ITI.Archi_Vite.Core
+﻿using System.Collections.Generic;
+
+namespace ITI.Archi_Vite.Core
 {
-    internal class Message
+    public class Message
     {
+        readonly string title;
+        readonly string contents;
+        readonly List<Person> receiver;
+        readonly Person sender;
+        public Message(string Intitulé, string Contenu, List<Person> Recepteurs, Person Expediteur)
+        {
+            title = Intitulé;
+            contents = Contenu;
+            sender = Expediteur;
+            receiver = Recepteurs;
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+        }
+
+        public string Contents
+        {
+            get
+            {
+                return contents;
+            }
+        }
+
+        public List<Person> Receiver
+        {
+            get
+            {
+                return receiver;
+            }
+        }
+
+        public Person Sender
+        {
+            get
+            {
+                return sender;
+            }
+        }
     }
 }
