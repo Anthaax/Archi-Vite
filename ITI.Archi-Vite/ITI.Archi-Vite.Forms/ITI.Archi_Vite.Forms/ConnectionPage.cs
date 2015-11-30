c0b6a2787e7badf5817bff36a779fcb016536eab
Anthaax <guillaume.fimes@laposte.net>
2015-11-30 16:01:12 +0100

    modification xamarin

--- ITI.Archi-Vite/ITI.Archi-Vite.Forms/ITI.Archi_Vite.Forms/ConnectionPage.cs
+++ ITI.Archi-Vite/ITI.Archi-Vite.Forms/ITI.Archi_Vite.Forms/ConnectionPage.cs
@@ -12,12 +12,48 @@ namespace ITI.Archi_Vite.Forms
     {
         public ConnectionPage()
         {
+            Image logo = new Image
+            {
+                Source = "Archi'Vite Logo.png",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.StartAndExpand
+            };
+            Entry pseudo = new Entry
+            {
+                Placeholder = "Pseudo",
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            Entry password = new Entry
+            {
+                Placeholder = "Password",
+                IsPassword = true,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            Button send = new Button
+            {
+                Text = "Se connecter",
+                TextColor = Color.Blue,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.End
+            };
+            send.Clicked += OnButtonClicked;
             Content = new StackLayout
             {
+
                 Children = {
-                    new Label { Text = "Hello ContentPage" }
+                    logo,
+                    pseudo,
+                    password,
+                    send
                 }
             };
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
