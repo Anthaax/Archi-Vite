d757c045f33ed6ed934ed351f2d69909886932ba
Anthaax <guillaume.fimes@laposte.net>
2015-12-01 13:26:45 +0100

    Travail du matin

--- ITI.Archi-Vite/ITI.Archi-Vite.Forms/ITI.Archi_Vite.Forms/View/PatientPage.cs
+++ ITI.Archi-Vite/ITI.Archi-Vite.Forms/ITI.Archi_Vite.Forms/View/PatientPage.cs
@@ -12,76 +12,70 @@ namespace ITI.Archi_Vite.Forms
     {
         public PatientPage()
         {
-			//Label topLeftText;
-			Label topLeftLabel, centerLabel, bottomRightLabel;
+             Image logo = new Image
+            {
+                Source = "Logo.png",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.StartAndExpand
+            };
+            Entry pseudo = new Entry
+            {
+                Placeholder = "Pseudo",
+                FontSize = 40,
+                HorizontalTextAlignment = TextAlignment.Center,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.Gray
+            };
+            pseudo.TextChanged += EntryTextChanged;
+            Entry password = new Entry
+            {
+                Placeholder = "Password",
+                IsPassword = true,
+                FontSize = 40,
+                HorizontalTextAlignment = TextAlignment.Center,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+            password.TextChanged += EntryTextChanged;
+            Button send = new Button
+            {
+                Text = "Se connecter",
+                FontSize = 40,
+                BackgroundColor = Color.FromHex("439DFE"),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.End
+            };
+            send.Clicked += (sender, e) =>
+            {
+                throw new NotImplementedException();
 
-			public AbsoluteLayoutDemoPage ()
-			{
-				Label header = new Label {
-					Text = "AbsoluteLayout Demo",
-					Font = Font.SystemFontOfSize (NamedSize.Large),
-					HorizontalOptions = LayoutOptions.Center
-				};
+            };
+            Content = new StackLayout
+            {
 
-				AbsoluteLayout simpleLayout = new AbsoluteLayout {
-					BackgroundColor = Color.Blue.WithLuminosity (0.9),
-					VerticalOptions = LayoutOptions.FillAndExpand
-				};
+                Children = {
+                    logo,
+                    pseudo,
+                    password,
+                    send,
+                }
+            };
+            this.BackgroundColor = Color.White;
+        }
 
-				topLeftLabel = new Label {
-					Text = "Top Left",
-					TextColor = Color.Black
-				};
+        private void EntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = sender as Entry;
+            if (entry != null)
+            {
+                entry.TextColor = Color.Gray;
+            }
+        }
 
-				centerLabel = new Label {
-					Text = "Centered",
-					TextColor = Color.Black
-				};
-
-				bottomRightLabel = new Label { 
-					Text = "Bottom Right",
-					TextColor = Color.Black
-				};
-
-
-				// PositionProportional flag maps the range (0.0, 1.0) to
-				// the range "flush [left|top]" to "flush [right|bottom]"
-				AbsoluteLayout.SetLayoutFlags (bottomRightLabel,
-					AbsoluteLayoutFlags.PositionProportional);
-
-				AbsoluteLayout.SetLayoutBounds (topLeftLabel,
-					new Rectangle (0f,
-						0f, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
-
-				AbsoluteLayout.SetLayoutFlags (centerLabel,
-					AbsoluteLayoutFlags.PositionProportional);
-
-				AbsoluteLayout.SetLayoutBounds (centerLabel,
-					new Rectangle (0.5,
-						0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
-
-				AbsoluteLayout.SetLayoutFlags (bottomRightLabel,
-					AbsoluteLayoutFlags.PositionProportional);
-
-				AbsoluteLayout.SetLayoutBounds (bottomRightLabel,
-					new Rectangle (1f,
-						1f, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
-
-				simpleLayout.Children.Add (topLeftLabel);
-				simpleLayout.Children.Add (centerLabel);
-				simpleLayout.Children.Add (bottomRightLabel);
-
-				// Accomodate iPhone status bar.
-				this.Padding = 
-					new Thickness (10, Device.OnPlatform (20, 0, 0), 10, 5);
-
-				// Build the page.
-				this.Content = new StackLayout {
-					Children = {
-						header,
-						simpleLayout
-					}
-				};
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
