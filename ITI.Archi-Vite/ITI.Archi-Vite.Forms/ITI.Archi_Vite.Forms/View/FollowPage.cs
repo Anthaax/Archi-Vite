114eb22eb9f9a3ed5a6898a471a6934456e1fe14
Anthaax <guillaume.fimes@laposte.net>
2015-12-07 08:14:24 +0100

    Mes suivis

---
+++ ITI.Archi-Vite/ITI.Archi-Vite.Forms/ITI.Archi_Vite.Forms/View/FollowPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ITI.Archi_Vite.Forms
+{
+	public class FollowPage : ContentPage
+	{
+		public FollowPage ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
